9e0ab954354871ee470634671ef799d9c771dbd2
HarshPratapSingh1 <harshpratap715@gmail.com>
2026-01-12 23:54:54 +0530

    Fix: remove nested git repository

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center bg-gray-900">
+      <h1 className="text-5xl font-bold text-pink-500">
+        React + Tailwind is working 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
